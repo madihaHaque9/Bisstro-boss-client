9b852ba31872384ebcfb73bf883a4918623af7d8
madihaHaque9 <madia.haque@gmail.com>
2024-03-27 23:59:22 +0600

    featured section

--- src/pages/Shared/Featured/Featured.jsx
+++ src/pages/Shared/Featured/Featured.jsx
@@ -4,7 +4,7 @@ import './featured.css'
 
 const Featured = () => {
     return (
-        <div className="featured-item text-white mt-10">
+        <div className="featured-item text-white mt-10 bg-fixed">
           <SectionTitle subHeading="Check It Out" heading="Featured Item"></SectionTitle>
           <div className="md:flex justify-center items-center pb-20 pt-12 px-16 ">
             <div>
@@ -13,7 +13,7 @@ const Featured = () => {
                     <p>Aug 20,2024</p>
                     <p className="uppercase">Where Can I Get Some?</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, saepe tempora reiciendis similique molestiae odit exercitationem non porro dolorum? Ex placeat iusto molestias cupiditate consectetur aliquam quisquam facilis! Aperiam delectus praesentium dolorem est nostrum consectetur fugit nisi, minima optio expedita iste saepe quidem commodi, voluptatibus aliquam quos exercitationem ducimus adipisci.</p>
-                    <button className="btn btn-outline btn-info">Order Now</button>
+                    <button className="btn btn-outline btn-info border-0 border-b-4">Order Now</button>
                 </div>
                 </div>  
         </div>
