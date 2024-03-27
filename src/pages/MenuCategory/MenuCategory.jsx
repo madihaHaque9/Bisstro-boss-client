f522ee58e0d6075e77424b7031d9bd4ab6552ae5
madihaHaque9 <madia.haque@gmail.com>
2024-03-28 05:50:40 +0600

    tab created

--- src/pages/MenuCategory/MenuCategory.jsx
+++ src/pages/MenuCategory/MenuCategory.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Cover from "../Shared/Cover/Cover";
 import MenuItem from "../Shared/MenuItem/MenuItem";
 
@@ -12,6 +13,9 @@ const MenuCategory = ({items,title,img}) => {
                     <MenuItem key={item._id}
                      item={item}></MenuItem>)
                 }
+                <Link to={`/order/${title}`}>
+                <button className="btn btn-outline btn-info border-0 border-b-4">Order Now</button>
+                </Link>
             </div>
         </div>
        
