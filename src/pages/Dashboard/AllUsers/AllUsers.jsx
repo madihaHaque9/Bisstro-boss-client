89a15f2cc167615a4626b256b2b41e6205663fcd
madihaHaque9 <madia.haque@gmail.com>
2024-03-30 01:20:48 +0600

    create jwt token

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,16 +1,62 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { IoIosTrash } from "react-icons/io";
+import { ImAccessibility } from "react-icons/im";
+import Swal from "sweetalert2";
 
 
 const AllUsers = () => {
     const axiosSecure=useAxiosSecure();
-    const {data:users=[]}=useQuery({
+    const {data:users=[],refetch}=useQuery({
         queryKey:['users'],
         queryFn: async()=>{
             const res=await axiosSecure.get('/users');
             return res.data;
         }
     })
+    const handleMakeAdmin=user=>{
+        axiosSecure.patch(`/users/admin/${user._id}`)
+        .then(res=>{
+            console.log(res.data)
+            if(res.data.modifiedCount>0){
+                refetch();
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `${user.name}is admin now`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+            }
+        })
+
+    }
+    const handledeleteUser = user => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+  
+                axiosSecure.delete(`/users/${user._id}`)
+                .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        })
+    }
     return (
         <div>
         <div className="flex justify-evenly my-4">
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { app } from "../Firebase/firebase.config";
 import {  GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 export const AuthContext=createContext(null)
 
 const auth = getAuth(app);
@@ -8,6 +9,7 @@ const AuthProvider = ({children}) => {
     const [user,setUser]=useState(null)
     const[loading,setLoading]=useState(true);
     const googleProvider=new GoogleAuthProvider();
+    const axiosPublic=useAxiosPublic();
     const createUser=(email,password)=>{
         setLoading(true);
         return createUserWithEmailAndPassword(auth,email,password)
@@ -33,7 +35,18 @@ const AuthProvider = ({children}) => {
     useEffect(()=>{
       const unsubscribe=  onAuthStateChanged(auth,currentUser=>{
             setUser(currentUser);
-            console.log('current user',currentUser);
+            if(currentUser){
+                const userInfo={email:currentUser.email};
+             axiosPublic.post('/jwt',userInfo)
+             .then(res=>{
+              if ( res.data.token){
+                localStorage.setItem('access-token',res.data.token)
+              }
+             })
+            }
+            else{
+              localStorage.removeItem('access-token')
+            }
             setLoading(false);
         });
         return ()=>{
